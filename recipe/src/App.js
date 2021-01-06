a29022e7b0e886c60a16638799970ba8dd1db679
Mumbi <veronicmuthiga@gmail.com>
2021-01-06 16:48:52 +0300

    Add the search functionalityt

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -8,40 +8,55 @@ const App=()=>{
   
 
   const [recipes ,setRecipes]=useState([])
+  const [search ,setSearch]=useState('');
+  const [query ,setQuery]=useState('chicken');
   
 
 
 useEffect(()=>{
   getRecipes();
 
-} ,[])
+} ,[query])
 
 const getRecipes=async ()=>{
-  const response=await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const response=await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
   const data= await response.json();
   setRecipes(data.hits)
   console.log(data.hits)
 
 };
+const updateSearch=e=>{
+setSearch(e.target.value);
+console.log(search)
+}
+const getSearch=e=>{
+  e.preventDefault();
+  setQuery(search);
+  setSearch('');
+}
 
   return(
     <div className="App">
-    <h1>Hello Recipe</h1>
-    <form className="search-form">
-    <input type="text" className="search-bar"></input>
+   
+    <form onSubmit={getSearch} className="search-form">
+    <input type="text" className="search-bar" value={search} onChange={updateSearch}></input>
     <button  className="search-button">Search</button>
     </form>
+    <div className="recipes">
     {recipes.map(recipe=>(
 
       <Recipe 
-      key={recipe.recipe.label}
+     
       title={recipe.recipe.label}
       calories={recipe.recipe.calories}
       image={recipe.recipe.image}
+      ingredients={recipe.recipe.ingredients}
       >
+     
       
       </Recipe>
     ))}
+    </div>
     
     </div>
   );
--- recipe/src/Recipe.js
+++ recipe/src/Recipe.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-export default function Recipe({title ,calories ,image}) {
+export default function Recipe({title ,calories ,image ,ingredients}) {
     return (
         <div>
             <h1>{title}</h1>
-            <p>{calories}</p>
+            <ol>
+            {ingredients.map(ingredient=>(
+                <li>{ingredient.text}</li>
+            ))}
+            </ol>
+            <h5>{calories} calories</h5>
             <img src={image} alt=""></img>
+
         </div>
     )
 }
